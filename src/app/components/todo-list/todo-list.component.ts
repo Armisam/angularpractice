629a5219d7d101f799b09983e4e9ca892d977bb1
Székely Csaba <csabaszekely99@gmail.com>
2025-09-11 12:46:27 +0200

    angular updated to 19

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { TodoListService } from '../../services/todo-list.service';
 
 @Component({
-  selector: 'app-todo-list',
-  templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
+    styleUrls: ['./todo-list.component.scss'],
+    standalone: false
 })
 
 export class TodoListComponent implements OnInit {
