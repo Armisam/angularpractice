10ac646b738d0be2ea210ee9a21722e18f242069
Székely Csaba <csabaszekely99@gmail.com>
2025-09-11 11:03:09 +0200

    angular updated to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoListService } from './services/todo-list.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    TodoListComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [TodoListService],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CounterComponent,
+        TodoListComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule], providers: [TodoListService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
